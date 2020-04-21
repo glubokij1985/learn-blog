ad74ac0d3020a78eda840127d94c2d6b4e7b0d63
front-boombox <glubokij85@gmail.com>
2020-04-21 23:55:21 +0300

    added login page with validation

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/interfaces';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  public submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+  }
+
+}
